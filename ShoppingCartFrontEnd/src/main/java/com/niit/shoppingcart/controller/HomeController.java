211ca911f6785aa2f9b1744f2908f0e3f13d60ef
Saleemahmh <ssaleemah24@gmail.com>
2016-10-21 23:26:39 +0530

    this is my third commit

--- ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/controller/HomeController.java
+++ ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/controller/HomeController.java
@@ -3,15 +3,40 @@ package com.niit.shoppingcart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+	
+	@RequestMapping("/")
+	public String gotoHome()
+	{
+		return "index";
+	}
+	
+	@RequestMapping("/validate")
+	public String validate(@RequestParam(name="userID") String id,
+			@RequestParam (name="password") String pwd
+			, Model model)
+	{   
+		//Validate - hit the database to validate-
+		//NIIT
+		if(id.equals("niit") && pwd.equals("niit")){
+			model.addAttribute("successMessage","You successfully logged in");
+			return"index";
+		}
+		else
+		{
+			model.addAttribute("errorMessage","Invalid Credentials....pls type correctly");
+			return"login";
+		}//SPA
+		
+	}
 
 	@RequestMapping("/Login")
 	public String login(Model model) 
 	{
-		//String name="NIIT"
-		//${name}  -Spring EL - Expression language
+		
 		//Boolean userClickedLogin=true
 		model.addAttribute("userClickedLogin","true");
 		return "login";
